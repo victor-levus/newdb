11c9a3068134d156072c64bd599fd734878b5012
victorlevus <victorlevus@mail.ru>
2022-12-11 18:56:32 +0100

    prepare for production

--- database/settings/prod.py
+++ database/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .common import *
 
 
@@ -8,5 +9,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['levus-training.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
