7c7cad94ce38c578179a65cbd7ed92e0f6bfc677
victorlevus <victorlevus@mail.ru>
2023-03-20 21:58:15 +0100

    add allowed host and config env var

--- database/settings/prod.py
+++ database/settings/prod.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['levus-training.herokuapp.com']
+ALLOWED_HOSTS = ['super-mart2.herokuapp.com/']
 
 # CSRF_TRUSTED_ORIGINS = ['https://levus-training.herokuapp.com', "http://localhost:3000",
 #     "http://127.0.0.1:3000" ]
